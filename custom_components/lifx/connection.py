b2a0e91d20b18062ca8a8a997387e4bf54a2b252
Avi Miller <me@dje.li>
2023-01-03 12:09:42 +1100

    refactor: control the amount of inflight messages
    Also allow more tolerance for empty responses.
    
    Signed-off-by: Avi Miller <me@dje.li>

--- custom_components/lifx/connection.py
+++ custom_components/lifx/connection.py
@@ -8,7 +8,7 @@ from aiolifx.aiolifx import UDP_BROADCAST_PORT, Light
 from aiolifx.message import Message
 import async_timeout
 
-from .const import _LOGGER, OVERALL_TIMEOUT, TARGET_ANY
+from .const import OVERALL_TIMEOUT, TARGET_ANY
 
 
 class LIFXCustomConnection:
@@ -21,6 +21,7 @@ class LIFXCustomConnection:
         self.device = None
         self.transport = None
         self._must_reconnect: bool = False
+        self._inflight_limit = asyncio.Semaphore(4)
 
     async def async_setup(self):
         """Ensure we are connected."""
@@ -34,36 +35,21 @@ class LIFXCustomConnection:
         """Call aiolifx methods asynchronously."""
 
         if self.transport is None or self._must_reconnect is True:
-            _LOGGER.debug("Creating new connection to %s (%s)", self.host, self.mac)
-            loop = asyncio.get_running_loop()
-            self.transport, self.device = await loop.create_datagram_endpoint(
-                lambda: Light(loop, self.mac, self.host),
-                remote_addr=(self.host, UDP_BROADCAST_PORT),
-            )
-            self._must_reconnect = False
+            # close the current connection
+            self.async_stop()
+            # then open a new one
+            await self.async_setup()
 
         response: asyncio.Future[tuple[Light, Message]] = asyncio.Future()
 
         def _aiolifx_callback(light: Light, message: Message) -> None:
             """Process the response from aiolifx."""
-            if message is None and not response.done():
-                _LOGGER.warning(
-                    "Empty repy received from %s (%s)", light.label, light.ip_addr
-                )
-                response.set_result((light, None))
-
             if (
                 light.mac_addr == TARGET_ANY
                 and message is not None
                 and message.target_addr != TARGET_ANY
             ):
                 light.mac_addr = message.target_addr
-                _LOGGER.debug(
-                    "Set mac address of %s (%s) to %s",
-                    light.label,
-                    light.ip_addr,
-                    light.mac_addr,
-                )
 
             if not response.done():
                 response.set_result((light, message))
@@ -71,25 +57,28 @@ class LIFXCustomConnection:
         light: Light = None
         message: Message = None
 
-        method(callb=_aiolifx_callback)
-        async with async_timeout.timeout(delay=OVERALL_TIMEOUT):
-            light, message = await response
+        async with self._inflight_limit:
 
-        while len(self.device.message) > 0:
-            await asyncio.sleep(0)
+            method(callb=_aiolifx_callback)
+            async with async_timeout.timeout(delay=OVERALL_TIMEOUT):
+                light, message = await response
 
-        if message is None:
-            self.async_stop()
-            self._must_reconnect = True
-            _LOGGER.debug("Closed connection to %s (%s)", self.host, self.mac)
-            raise asyncio.TimeoutError(
-                f"No response from {light.label} ({light.ip_addr}"
-            )
+            while len(self.device.message) > 0:
+                await asyncio.sleep(0)
+
+            if message is None:
+                raise asyncio.TimeoutError(
+                    f"No response from {light.label} ({light.ip_addr}) [{light.mac_addr}]"
+                )
 
         return message
 
     def async_stop(self):
         """Close the transport."""
-        assert self.transport is not None
-        self.transport.close()
-        self.transport = None
+        if self.device is not None and isinstance(self.device.task, asyncio.Task):
+            self.device.task.cancel()
+            self.device.task = None
+
+        if self.transport is not None and isinstance(self.transport, asyncio.Transport):
+            self.transport.close()
+            self.transport = None
